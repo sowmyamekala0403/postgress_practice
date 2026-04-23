549882b89cfd34438146a2f9b8ad2405f2ce7acc
Sowmya_Mekala <127084093+sowmyamekala0403@users.noreply.github.com>
2026-04-23 22:56:27 +0530

    added with feature which in turn automatically closes connection.

--- python_Postgress.py
+++ python_Postgress.py
@@ -12,79 +12,79 @@ cur = None
 
 try:
     # connection creation
-    conn = psycopg2.connect(
+    with psycopg2.connect(
         host=hostname,
         dbname=dbname,
         user=username,
         password=pwd,
         port=port_id
-    )
+    ) as conn:
 
-    # cursor creation
-    cur = conn.cursor(cursor_factory=extras.DictCursor)
+        # cursor creation
+        with conn.cursor(cursor_factory=extras.DictCursor) as cur:
 
-    # DROP TABLE
-    cur.execute("DROP TABLE IF EXISTS employee")
-
-    # CREATE TABLE
-    create_script = '''
-    CREATE TABLE IF NOT EXISTS employee (
-        id INT PRIMARY KEY,
-        name VARCHAR(40) NOT NULL,
-        salary INT,
-        dept_id VARCHAR(20)
-    )
-    '''
-    cur.execute(create_script)
-
-    # INSERT DATA
-    insert_script = '''
-    INSERT INTO employee (id, name, salary, dept_id)
-    VALUES (%s, %s, %s, %s)
-    '''
-    insert_values = [
-        (1, "sowmya", 10000, "HR"),
-        (2, "ram", 15000, "IT"),
-        (3, "krishna", 20000, "FIN")
-    ]
-
-    for record in insert_values:
-        cur.execute(insert_script, record)
-
-    # UPDATE
-    update_script = '''
-    UPDATE employee
-    SET salary = salary * 10 + salary
-    WHERE id = 1
-    '''
-    cur.execute(update_script)
-
-    # DELETE
-    delete_script = '''
-    DELETE FROM employee
-    WHERE name = %s
-    '''
-    cur.execute(delete_script, ("sowmya",))
-
-    # SELECT query
-    cur.execute("SELECT * FROM employee")
-
-    # fetch all rows
-    rows = cur.fetchall()
-
-    # print data
-    for row in rows:
-        print(row["id"], row["name"], row["salary"], row["dept_id"])
-
-
-    # commit changes
-    conn.commit()
+            # DROP TABLE
+            cur.execute("DROP TABLE IF EXISTS employee")
+        
+            # CREATE TABLE
+            create_script = '''
+            CREATE TABLE IF NOT EXISTS employee (
+                id INT PRIMARY KEY,
+                name VARCHAR(40) NOT NULL,
+                salary INT,
+                dept_id VARCHAR(20)
+            )
+            '''
+            cur.execute(create_script)
+        
+            # INSERT DATA
+            insert_script = '''
+            INSERT INTO employee (id, name, salary, dept_id)
+            VALUES (%s, %s, %s, %s)
+            '''
+            insert_values = [
+                (1, "sowmya", 10000, "HR"),
+                (2, "harshi", 15000, "IT"),
+                (3, "krishna", 20000, "FIN")
+            ]
+        
+            for record in insert_values:
+                cur.execute(insert_script, record)
+        
+            # UPDATE
+            update_script = '''
+            UPDATE employee
+            SET salary = salary * 10 + salary
+            WHERE id = 1
+            '''
+            cur.execute(update_script)
+        
+            # DELETE
+            delete_script = '''
+            DELETE FROM employee
+            WHERE name = %s
+            '''
+            cur.execute(delete_script, ("sowmya",))
+        
+            # SELECT query
+            cur.execute("SELECT * FROM employee")
+        
+            # fetch all rows
+            rows = cur.fetchall()
+        
+            # print data
+            for row in rows:
+                print(row["id"], row["name"], row["salary"], row["dept_id"])
+        
+        
+            # commit changes
+            conn.commit()
 
 except Exception as e:
     print("Exception raised:", e)
 
-finally:
-    if cur is not None:
-        cur.close()
-    if conn is not None:
-        conn.close()
+# finally:
+#     if cur is not None:
+#         cur.close()
+#     if conn is not None:
+#         conn.close()
